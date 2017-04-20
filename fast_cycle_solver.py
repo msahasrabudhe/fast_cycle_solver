dd42ecdb98e4f410a5b692a74fbeb977ec67f2ec
msahasrabudhe <mihir.s.sahasrabudhe@gmail.com>
2017-04-20 09:55:04 +0200

    Initial commit.

---
+++ fast_cycle_solver.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+
+# Python implementation of a fast cycle solver [1]. 
+#
+#
+# References:
+# [1]: A Fast and Exact Energy Minimization Algorithm for Cycle MRFs, H. Wang, D. Koller, ICML 2013.
+
+
